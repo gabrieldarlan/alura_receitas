c7b57a34b8180305faceb282238b934ffb9d4d01
Gabriel Darlan Silva <gabriel.darlan88@gmail.com>
2020-10-16 17:55:38 -0300

    cap 04 aula 01 completa

--- receitas/views.py
+++ receitas/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete de chocolate',
+        4: 'Panqueca',
+        5: 'Ovo frito'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
+
+
+def receita(request):
+    return render(request, 'receita.html')
