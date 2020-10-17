596f3438094ef7c35212d7b8edd10513889f8d2f
Gabriel Darlan Silva <gabriel.darlan88@gmail.com>
2020-10-17 20:21:49 -0300

    cap 01 completo

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(
+        max_length=200, verbose_name='nome da receita')
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField(verbose_name='modo de preparo')
+    tempo_preparo = models.IntegerField(verbose_name='tempo de preparo')
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receita = models.DateTimeField(
+        default=datetime.now, verbose_name='data da receita', blank=True)
