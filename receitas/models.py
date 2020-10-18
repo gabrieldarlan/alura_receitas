10a43d2044a09f7485d795dd1b15eaa50b5af879
Gabriel Darlan Silva <gabriel.darlan88@gmail.com>
2020-10-18 00:16:34 -0300

    cap 03 completo

--- receitas/models.py
+++ receitas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(
         max_length=200, verbose_name='nome da receita')
     ingredientes = models.TextField()
@@ -12,3 +14,5 @@ class Receita(models.Model):
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(
         default=datetime.now, verbose_name='data da receita', blank=True)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
+    publicada = models.BooleanField(default=False)
