468ff82c58c21fc92d1eb3245a10d96569f56318
Gabriel Darlan Silva <gabriel.darlan88@gmail.com>
2020-10-18 09:58:59 -0300

    curso 02 completo

--- receitas/models.py
+++ receitas/models.py
@@ -16,3 +16,6 @@ class Receita(models.Model):
         default=datetime.now, verbose_name='data da receita', blank=True)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome_receita
